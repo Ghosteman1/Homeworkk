ffc9516e57102af33944e97e04b12c1caa2ed7e9
Ghosteman1 <dtxaratishvili.2002@gmail.com>
2023-09-28 23:58:09 +0400

    second commit.

---
+++ homework/day2.py
@@ -0,0 +1,8 @@
+mom_age = 38
+sister_age = 2
+brother_age = 9
+dad_age = 41
+print("In 5 years my mom's age will be",mom_age + 5)
+print("In 5 years my sister's age will be",sister_age + 5) 
+print("In 5 years my brother's age will be",brother_age + 5)
+print("In 5 years my dad's age will be",dad_age + 5)
